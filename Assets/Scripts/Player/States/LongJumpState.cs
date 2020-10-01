fb834f5b07d70e534f69387875427edbe4b28204
Antti M <antti_rekisteroinnit@hotmail.com>
2020-10-01 17:43:20 +0300

    Remove a Debug.Log and check that player is grounded before jumping

--- Assets/Scripts/Player/States/LongJumpState.cs
+++ Assets/Scripts/Player/States/LongJumpState.cs
@@ -8,15 +8,17 @@ public class LongJumpState : PlayerState
 
     protected override void CustomStartState()
     {
-        Debug.Log(PreviousState);
-        // Apply jump impulse depending on the previous state.
-        playerRigidBody.velocity = new Vector2(0.0f, playerRigidBody.velocity.y);
-        MoveState prevState = PreviousState as MoveState;
-        // In some case, one might want to set the jump force to 0 but we don't expect that.
-        if (prevState && prevState.CustomJumpForce.sqrMagnitude > 0.01f)
-            playerRigidBody.AddForce(prevState.CustomJumpForce, ForceMode2D.Impulse);
-        else
-            playerRigidBody.AddForce(defaultJumpForce, ForceMode2D.Impulse);
+        if (PlayerController.Instance.Grounded)
+        {
+            // Apply jump impulse depending on the previous state.
+            playerRigidBody.velocity = new Vector2(0.0f, playerRigidBody.velocity.y);
+            MoveState prevState = PreviousState as MoveState;
+            // In some case, one might want to set the jump force to 0 but we don't expect that.
+            if (prevState && prevState.CustomJumpForce.sqrMagnitude > 0.01f)
+                playerRigidBody.AddForce(prevState.CustomJumpForce, ForceMode2D.Impulse);
+            else
+                playerRigidBody.AddForce(defaultJumpForce, ForceMode2D.Impulse);
+        }
     }
 
     protected override void CustomStateUpdate()
