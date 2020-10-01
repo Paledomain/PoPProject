a022abb6acd9adf818f1e8e41cdda0785852ec69
Kaan ÜNLÜ <47030330+Paledomain@users.noreply.github.com>
2020-10-02 02:44:03 +0300

    add spike trap
    also small player speed changes and placeholder trap sprites and anims

---
+++ Assets/Scripts/Traps/SpikeTrap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    bool inside = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Char"))
+        {
+            inside = true;
+            if (other.gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Artemis_Run"))
+            {
+                Debug.Log("haha get spike trapped");
+                if (inside)
+                    this.gameObject.GetComponent<Animator>().SetBool("pressed", true);
+                Destroy(other.gameObject);
+            }
+            inside = false;
+        }
+        else
+            inside = false;
+    }
+
+}
