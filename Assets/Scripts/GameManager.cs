f8b73c2456d25429d43703b232be12a9fbb9f596
Antti M <antti_rekisteroinnit@hotmail.com>
2020-10-13 16:37:55 +0300

    Restart game with R or Esc.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (InputMapper.Instance.GetKey(GameButton.Restart))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
