4f40d14e02e126115c6c628236b4fdec58e5dec8
Antti M <antti_rekisteroinnit@hotmail.com>
2020-10-14 18:43:41 +0300

    Pressure plate + gate.

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField]
+    private float openDuration = 5.0f;
+
+    private Animator animator;
+    private float plateUnpressedTime = -1.0f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (plateUnpressedTime > 0.0f && Time.time - plateUnpressedTime > openDuration)
+        {
+            animator.SetBool("openGate", false);
+        }
+    }
+
+    public void OnPressurePlatePressed()
+    {
+        animator.SetBool("openGate", true);
+        plateUnpressedTime = -1.0f;
+    }
+
+    public void OnPressurePlateUnPressed()
+    {
+        plateUnpressedTime = Time.time;
+    }
+}
---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField]
+    private Gate connectedGate;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!connectedGate)
+        {
+            Debug.LogWarning("Missing the animator to trigger.");
+            return;
+        }
+
+        if (collision.CompareTag("Char"))
+        {
+            connectedGate.OnPressurePlatePressed();
+        }
+        animator.SetBool("pressed", true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!connectedGate)
+        {
+            Debug.LogWarning("Missing the animator to trigger.");
+            return;
+        }
+
+        if (collision.CompareTag("Char"))
+        {
+            connectedGate.OnPressurePlateUnPressed();
+        }
+        animator.SetBool("pressed", false);
+    }
+}
