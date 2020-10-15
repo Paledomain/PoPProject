ce3c36a5cd0153bf73186370fea8f7bcf09a7e0c
Antti M <antti_rekisteroinnit@hotmail.com>
2020-10-15 11:14:06 +0300

    Skeleton for time limit.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private float timeLimitSeconds = 240.0f;
+    [SerializeField]
+    TMP_Text timeLeftText;
+
+    private float endOfTime;
+
+    private void Start()
+    {
+        endOfTime = Time.time + timeLimitSeconds;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -10,6 +23,16 @@ public class GameManager : MonoBehaviour
         {
             Scene scene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(scene.name);
+        }
+
+        float timeRemaining = endOfTime - Time.time;
+        if (timeRemaining > 0 && timeLeftText)
+        {
+            timeLeftText.text = Mathf.CeilToInt(timeRemaining).ToString();
+        }
+        else if (timeRemaining < 0)
+        {
+            // Show end screen (win or lose).
         }
     }
 }
