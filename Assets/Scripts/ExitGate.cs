971f4a2da2bca6e4d065657fac627bcb7a6e9678
Antti M <antti_rekisteroinnit@hotmail.com>
2020-10-15 12:02:15 +0300

    Add level exit.

---
+++ Assets/Scripts/ExitGate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGate : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Char"))
+        {
+            LevelEnd.Instance.ShowEndScreen(GameManager.Instance.GameTime, true, false);
+        }
+    }
+}
